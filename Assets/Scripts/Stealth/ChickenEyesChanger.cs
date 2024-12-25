d0f53b8a43812568b3f7b245b5ea56bcc45477fb
Danylo Liubymyi <denedi2004@gmail.com>
2024-12-25 22:55:13 +0100

    Red chicken eyes added.

--- Assets/Scripts/Stealth/ChickenEyesChanger.cs
+++ Assets/Scripts/Stealth/ChickenEyesChanger.cs
@@ -1,9 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Stealth
 {
     public class ChickenEyesChanger : MonoBehaviour
     {
-        
+        [SerializeField] private GameObject eyeMesh;
+        [SerializeField] private ChickenNavigation chickenNavigation;
+
+        private void OnValidate()
+        {
+            chickenNavigation = GetComponent<ChickenNavigation>();
+        }
+
+        public void FixedUpdate()
+        {
+            if (chickenNavigation.GetCurrentState != ChickenStates.Patroling)
+            {
+                eyeMesh.SetActive(true);
+            }
+            else
+            {
+                eyeMesh.SetActive(false);
+            }
+        }
     }
 }
